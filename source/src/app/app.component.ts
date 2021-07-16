efe7863f482b9d885f801ee5264810c88fd02b41
Vasu Agarwal <vasu2995@gmail.com>
2021-07-17 03:11:20 +0530

    First revamp: added title landing page

--- source/src/app/app.component.ts
+++ source/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-app';
+  list = ['a software developer', 'a software engineer', 'an aspiring ML engineer'];
+
+  // @Output() title_desc = '|';
+  // ngOnInit() {
+  //   setTimeout(() => {
+  //     this.typeWriter_write('a software developer', 0);
+  //   }, 800);
+  // }
+
+  // typeWriter_write(txt: string, i: number) {
+  //   console.log('write');
+  //   const speed = 90;
+  //   if (i < txt.length) {
+  //     this.title_desc = [this.title_desc.slice(0, this.title_desc.length - 1), txt.charAt(i), this.title_desc.slice(this.title_desc.length - 1)].join('');
+  //     i++;
+  //     setTimeout(() => { this.typeWriter_write(txt, i); }, speed);
+  //   }
+  // }
+
+  // typeWriter_delete() {
+  //   console.log('delete');
+  //   const speed = 30;
+  //   while (this.title_desc.length > 2) {
+  //     setTimeout(() => {
+  //       this.title_desc = this.title_desc.slice(this.title_desc.length - 2) + this.title_desc[this.title_desc.length - 1];
+  //       console.log(this.title_desc);
+  //     }, speed);
+  //   }
+  // }
 }
--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularTypewriterEffectModule } from 'angular-typewriter-effect';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularTypewriterEffectModule,
     AppRoutingModule
   ],
   providers: [],
